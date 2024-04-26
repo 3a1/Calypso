561f52ee26b2fa12fb300f76ca29767d51d19b19
Zebra <ratunke@gmail.com>
2024-04-26 07:10:36 +0200

    offsets updated

--- CalypsoUM/offsets.h
+++ CalypsoUM/offsets.h
@@ -5,14 +5,14 @@
 namespace offsets
 {
     // these offsets below are almost always change after a game update
-    constexpr std::ptrdiff_t dwEntityList = 0x18C1DE8;
-    constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x17361E8;
-    constexpr std::ptrdiff_t dwViewMatrix = 0x19231E0;
-    constexpr std::ptrdiff_t dwViewAngles = 0x192F970;
+    constexpr std::ptrdiff_t dwEntityList = 0x18C6268;
+    constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x173A3C8;
+    constexpr std::ptrdiff_t dwViewMatrix = 0x19278B0;
+    constexpr std::ptrdiff_t dwViewAngles = 0x19342F0;
 
 
     // these offsets below are often the same after update
-    constexpr std::ptrdiff_t m_iIDEntIndex = 0x15A4;
+    constexpr std::ptrdiff_t m_iIDEntIndex = 0x13B0;
     constexpr std::ptrdiff_t m_vecViewOffset = 0xC58;
     constexpr std::ptrdiff_t m_hPlayerPawn = 0x7E4;
     constexpr std::ptrdiff_t m_lifeState = 0x338;
@@ -23,7 +23,7 @@ namespace offsets
 
     constexpr std::ptrdiff_t m_pGameSceneNode = 0x318;
     constexpr std::ptrdiff_t m_modelState = 0x160;
-    constexpr std::ptrdiff_t dwNetworkGameClient = 0x514AC8;
+    constexpr std::ptrdiff_t dwNetworkGameClient = 0x515AC8;
     constexpr std::ptrdiff_t dwNetworkGameClient_getMaxClients = 0x250;
     constexpr std::ptrdiff_t dwNetworkGameClient_signOnState = 0x240;
 }
